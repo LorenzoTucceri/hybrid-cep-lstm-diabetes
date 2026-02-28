1431ac5bb7713e1d290276720930bacb9efc6b2d
LorenzoTucceri <64541311+LorenzoTucceri@users.noreply.github.com>
2026-02-28 14:22:04 +0100

    Revert "Merge branch 'dev-davide' of https://github.com/LorenzoTucceri/ISEQL into dev-davide"
    This reverts commit d3b1fa71ca27d43e9cc2afc1bb491c183e255557, reversing
    changes made to d1799d2579224f9ff34267eae64fb484a7439429.

--- laravel-iseql/app/Providers/AppServiceProvider.php
+++ laravel-iseql/app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\URL;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -26,10 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        //
         Schema::defaultStringLength(191);
 
-        if (request()->header("x-forwarded-proto") === "https") {
-            URL::forceScheme("https");
-        }
     }
 }
